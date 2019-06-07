5725e221e563a50d139159ee141ca8c4ae178ec3
党学义 <dangxy99@163.com>
2019-06-07 22:12:58 +0800

    interface

--- app/src/main/java/com/example/kotlinproject/kotlinInterface.kt
+++ app/src/main/java/com/example/kotlinproject/kotlinInterface.kt
@@ -1,36 +1,37 @@
 package com.example.kotlinproject
 
-open class  Parent{
-   open fun name(){
-       println("parent")
+open class Parent {
+    open fun name() {
+        println("parent")
     }
 }
 
-class Child : Parent(){
-    override fun name(){
+class Child : Parent() {
+    override fun name() {
         super.name()
         println("child")
     }
 
 }
 
-interface Grand{
-    fun name(){
+interface Grand {
+    fun name() {
         println("grand")
     }
 }
 
 
-class  Child2 : Parent(),Grand{
+class Child2 : Parent(), Grand {
     override fun name() {
         super<Grand>.name()
         super<Parent>.name()
         println("child")
     }
 }
+
 fun main(args: Array<String>) {
     var child = Child()
-    //  child.name()
+    child.name()
 
     var child2 = Child2()
     child2.name()
